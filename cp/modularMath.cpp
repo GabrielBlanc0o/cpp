d81e420f80feff4331bfdec0799e0d3ccd2f2776
GabrielBlanc0o <gtbvacation@outlook.es>
2026-06-03 10:35:53 -0500

    modular math introduction of the book

--- cp/modularMath.cpp
+++ cp/modularMath.cpp
@@ -1,7 +1,57 @@
 /*
-
 when smt says x mod m , this means when x is divided by m , 17 mod 5 = 2
 bc 17 = 3*5+2
-
-
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+// for cp we need short code 
+	// for example instead of writing long long we can use
+	typedef long long ll;
+
+	ll a = 123465789;
+	ll b = 987654321;
+	cout << a*b << endl;
+
+	// also for vectors of integers for vi
+	// and pi for a pair that contains two integers
+
+	typedef vector<int> vi;
+	typedef pair <int, int> pi;
+
+
+	/*
+	for cp we need good mathematical skills
+
+	sum formulas :
+
+	at page 20  where k is positive int has a closed form formula
+	that is a polynomial of degree k + 1 
+ 	is like 
+
+ 	sigma with n ahead and x = 1 and is equal to 
+ 	 1 + 2 + 3 ... +n = n(n+1)/2
+
+	and sigma with n ahead and x = x pow 2 and is equal to 
+	1 pow 2 + 2 pow 2 + 3 pow 2 ... +n pow 2 = n(n+1)(2n+1)/6
+
+
+	una progresion aritmetica es la secuencia de numeros done la diferencia
+	entre cualquiera de dos numeros consecutivos es constante:
+
+	por ejemplo 3,7,11,15
+
+	q seria  +4
+
+
+	*/
+
+
+
+
+
+}
